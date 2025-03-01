3b896b79812ba3fa9cea0b401c5762c3704ad7f6
Pujari Basvaraj <basvarajpujari607@gmail.com>
2025-03-01 23:07:55 +0530

    change: changed layout of app and code refcatoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Layout from './Layout/Layout'
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Layout from "./Layout/Layout";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-      <Layout/>
+        <Layout />
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
