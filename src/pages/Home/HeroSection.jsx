5e4a92c72e114cd0dad79079b98fa674575cd863
Pujari Basvaraj <basvarajpujari607@gmail.com>
2025-03-06 21:34:25 +0530

    change: changed mainsection Ui

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -2,54 +2,46 @@ import React from "react";
 import { ArrowRightIcon } from "../../Icons/Icons";
 import MainBanner from "../../../public/assets/featuredImages/main-banner.jpg";
 import { ArrowRight } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
     <div
-      className="flex min-h-screen bg-opacity-30 bg-no-repeat bg-center"
+      className="flex min-h-screen bg-no-repeat bg-center relative"
       style={{
         backgroundImage: `url(${MainBanner})`,
       }}
     >
-      <div className="text-white w-3/6 grid content-center pl-8">
-        <h1 className=" font-MontMedium text-7xl">
+      <div className="text-white w-3/6 absolute left-[50px] top-[90px] content-center pl-8">
+        <h1 className=" font-MontRegular text-7xl">
           New Age Of <br /> Modern Interior
         </h1>
-        <p className="font-MontMedium">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facere
-          minima voluptatibus temporibus! Laborum non, nisi fuga consequatur
-          alias officia.
+        <p className="font-MontRegular my-4">
+        Transform your space with our innovative designs that blend aesthetics with functionality. We create interiors that reflect your personality and elevate your lifestyle.
         </p>
-        <div className="flex my-12 gap-8">
-          <button className="flex justify-center items-center gap-2  pl-[0.85rem] pr-[0.65rem] py-[0.55rem] mr-2 mb-2 text-sm font-MontBold text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-transparent hover:text-white transition-all duration-700 cursor-pointer">
-            Read More
-            {/* <FontAwesomeIcon
-              className="self-end rounded-full text-[30px] font-[200] p-[9px] text-pink-100 transition-colors bg-black  focus:shadow-outline"
-              icon={faArrowRight}
-            /> */}
-            {/* <ArrowRightIcon size="15" /> */}
-            <span className="p-2 bg-black rounded-full">
-              <ArrowRight className="text-white text-[30px]" />
+        <div className="flex my-[1.5rem] gap-8">
+          <Link className="group flex items-center gap-2 rounded-full bg-white px-6 py-3 text-sm font-MontSemiBold text-black transition-all duration-300 hover:bg-white/90">
+            Explore More
+            <span className="flex h-8 w-8 items-center justify-center rounded-full bg-black transition-transform duration-300 group-hover:translate-x-1">
+              <ArrowRight className="h-4 w-4 text-white" />
             </span>
-            {/* <ArrowRight className="inline-block self-end rounded-full text-[30px] font-[200] text-white transition-colors bg-black  focus:shadow-outline " /> */}
-          </button>
-          <button className="font-MontBold flex justify-center items-center py-1 pl-6 pr-6 mr-2 mb-2 text-sm text-white focus:outline-none bg-transparent rounded-full border border-gray-200 hover:bg-white hover:text-black transition-all duration-700 cursor-pointer">
+          </Link>
+
+          <Link className="group flex items-center gap-2 rounded-full bg-white px-6 py-3 text-sm font-MontSemiBold text-black">
             Shop Now
-            {/* <FontAwesomeIcon
-              className=" ml-2 text-[20px]"
-              icon={faArrowRight}
-            /> */}
-            <ArrowRightIcon size="15" />
-          </button>
+            <span className="flex h-8 w-8 items-center justify-center rounded-full bg-black transition-transform duration-300 group-hover:translate-x-1">
+              <ArrowRight className="h-4 w-4 text-white" />
+            </span>
+          </Link>
         </div>
-        <div className="flex gap-10 divide-x-2 divide-gray-900">
+        <div className="flex gap-10 divide-x-2 divide-gray-950">
           <div className="">
             <span className="text-5xl font-MontMedium">270K+</span>
-            <p className="font-normal text-[18px]">Satisfied Customer</p>
+            <p className="font-MontSemiBold text-[18px]">Satisfied Customer</p>
           </div>
           <div className="pl-4">
             <span className="text-5xl font-MontMedium">1840K+</span>
-            <p className="font-normal text-[18px]">Orders Completed</p>
+            <p className="font-MontSemiBold text-[18px]">Orders Completed</p>
           </div>
         </div>
       </div>
