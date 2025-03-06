2d17f62c979cff4d0410cf6916de4942b52d2fb5
Pujari Basvaraj <basvarajpujari607@gmail.com>
2025-03-06 21:35:49 +0530

    change: Ui changes of footer content

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <div className="flex items-end w-full bg-white">
-        <footer className="w-full text-gray-700 bg-gray-100 body-font">
+        <footer className="w-full text-gray-700 bg-gray-100 font-MontRegular">
           <div className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
             <div className="flex flex-col items-center justify-center w-64">
               <a className="">
