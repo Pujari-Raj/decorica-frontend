7f0610f084c9d0bb281c3c741fc023ccabe7b4ef
Pujari Basvaraj <basvarajpujari607@gmail.com>
2025-03-14 21:24:50 +0530

    change: trying carousel transition

--- src/pages/Home/NewArrivals.jsx
+++ src/pages/Home/NewArrivals.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   CarouselProvider,
   Slider,
@@ -6,6 +6,7 @@ import {
   ButtonBack,
   ButtonNext,
 } from "pure-react-carousel";
+import { BackArrowIcon, NextArrowIcon } from "../../Icons/Icons";
 
 const slides = [
   { src: "./assets/featuredImages/white-vase.jpg", alt: "White Vase" },
@@ -17,7 +18,7 @@ const slides = [
   },
   {
     src: "./assets/featuredImages/hanging-light.jpg",
-    alt: "White Hanging Light",
+    alt: "Hanging Light",
   },
   {
     src: "https://images.unsplash.com/photo-1568482077316-1896b28e05d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
@@ -28,7 +29,7 @@ const slides = [
   { src: "./assets/featuredImages/wooden-frame.jpg", alt: "Wooden Frame" },
   {
     src: "./assets/featuredImages/white-frames.jpg",
-    alt: "White Portrait Frame",
+    alt: "Portrait Frames",
   },
   { src: "./assets/featuredImages/man-figurine.jpg", alt: "Man Figurine" },
   {
@@ -38,6 +39,17 @@ const slides = [
 ];
 
 const NewArrivals = () => {
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide+1) % slides.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [slides.length])
+  
+
   return (
     <div className="ml-4">
       <div className="text-center my-8 font-bold text-[#9d6a37]">
@@ -46,7 +58,7 @@ const NewArrivals = () => {
           Shop Before They Become Old
         </h1>
       </div>
-      <div className="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-start transition ease-out duration-1000">
+      <div className="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-center">
         <CarouselProvider
           className="lg:block hidden"
           naturalSlideWidth={100}
@@ -55,28 +67,13 @@ const NewArrivals = () => {
           visibleSlides={4}
           step={1}
           infinite={true}
+          currentSlide={currentSlide}
         >
           <div className="w-full relative flex items-center justify-center">
-            {/* Back Button */}
-            <ButtonBack className="absolute z-30 left-0 ml-8 focus:outline-none focus:bg-gray-400 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer">
-              <svg
-                width={8}
-                height={14}
-                viewBox="0 0 8 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 1L1 7L7 13"
-                  stroke="white"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </ButtonBack>
+            {/* <ButtonBack className="absolute z-30 left-0 ml-8 focus:outline-none focus:bg-gray-400 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer">
+              <BackArrowIcon/>
+            </ButtonBack> */}
 
-            {/* Slider */}
             <div className="w-full h-full mx-auto overflow-hidden">
               <Slider>
                 <div className="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-start transition ease-out duration-700">
@@ -89,7 +86,7 @@ const NewArrivals = () => {
                           className="object-cover object-center w-full"
                         />
                         <div className="bg-gray-800 bg-opacity-30 absolute w-full p-6 flex h-full items-end pb-6">
-                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white">
+                          <h3 className="text-xl lg:text-2xl font-MontMedium leading-5 lg:leading-6 text-white">
                             {slide.alt}
                           </h3>
                         </div>
@@ -99,25 +96,9 @@ const NewArrivals = () => {
                 </div>
               </Slider>
             </div>
-
-            {/* Next Button */}
-            <ButtonNext className="absolute z-30 right-0 mr-8 focus:outline-none focus:bg-gray-400 focus:ring-offset-2 focus:ring-gray-400">
-              <svg
-                width={8}
-                height={14}
-                viewBox="0 0 8 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 1L7 7L1 13"
-                  stroke="white"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </ButtonNext>
+            {/* <ButtonNext className="absolute z-30 right-0 mr-8 focus:outline-none focus:bg-gray-400 focus:ring-offset-2 focus:ring-gray-400">
+              <NextArrowIcon/>
+            </ButtonNext> */}
           </div>
         </CarouselProvider>
       </div>
