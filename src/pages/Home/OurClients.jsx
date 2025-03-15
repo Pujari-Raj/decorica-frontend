08ba9c076b1a8a929208a9697bcf70cdf94c56a6
Pujari Basvaraj <basvarajpujari607@gmail.com>
2025-03-15 13:21:22 +0530

    add: added react-fast-marquee pakcage for clients section and integrated it

--- src/pages/Home/OurClients.jsx
+++ src/pages/Home/OurClients.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 const clientLogos = [
   { image: "assets/brandlogos/amara.jpeg" },
@@ -11,8 +12,8 @@ const clientLogos = [
 
 const OurClients = () => {
   return (
-    <div className="ml-8 mr-8 mt-7">
-      <div className="text-center font-bold ">
+    <div className="">
+      <div className="text-center font-bold mt-7">
         <h2 className=" text-xl font-MontSemiBold text-[#9d6a37]">
           Trusted by Leading Home & Interior Brands
         </h2>
@@ -20,23 +21,26 @@ const OurClients = () => {
           Renowned brands trust us for home aesthetics.
         </h1>
       </div>
-      <div className="cards grid grid-cols-6 mt-4 gap-6 overflow-hidden">
-        {clientLogos.map((client, index) => (
-          <div className="text-[0.9rem]">
-            <a
-              key={index}
-              href="#"
-              className="text-[#5b5b5b] font-bold hover:text-[#e57200] hover:font-semibold"
-            >
-              <img
-                className="object-fill w-[15rem] h-[10rem] border-solid "
-                src={client.image}
-                alt={client.image}
-              />
-            </a>
-          </div>
-        ))}
-      </div>
+      <Marquee pauseOnHover={true} speed={45}>
+        <div className="cards grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 mt-4 gap-6 overflow-hidden">
+          {clientLogos.map((client, index) => (
+            <div className="text-[0.9rem]">
+              <a
+                key={index}
+                href="#"
+                className="text-[#5b5b5b] font-bold hover:text-[#e57200] hover:font-semibold"
+              >
+                <img
+                  className="object-fill w-[15rem] h-[10rem] border-solid "
+                  src={client.image}
+                  alt={client.image}
+                  loading="lazy" // explore what is this new attribute 
+                />
+              </a>
+            </div>
+          ))}
+        </div>
+      </Marquee>
     </div>
   );
 };
