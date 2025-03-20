e65c3858adff268f2cd4cb8b9f0901393fe8ba42
Pujari Basvaraj <basvarajpujari607@gmail.com>
2025-03-20 21:39:49 +0530

    add: added blog page

---
+++ src/pages/Blog/Blog.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    // <header className="w-full container mx-auto">
+    <div className="flex flex-col items-center py-12">
+      <p className="font-semibold text-gray-800 capitalize hover:text-gray-700 text-[2.65rem]">
+        {/* Blog Posts */}
+      </p>
+      <p className="font-MontSemiBold font-semibold text-[2rem] text-gray-600">The Decorica Journal</p>
+    </div>
+//   </header>
+  )
+}
+
+export default Blog
