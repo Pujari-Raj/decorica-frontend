5b492de085a953a8dbdd276754a18e11a1204381
Pujari Basvaraj <basvarajpujari607@gmail.com>
2025-04-11 23:26:37 +0530

    change: tweaks with globalconstant for testing

--- src/constants/globalConstants.js
+++ src/constants/globalConstants.js
@@ -3,33 +3,34 @@
 import React from "react";
 
 const Home = React.lazy(() => import("../pages/Home/Home"));
+const Blog = React.lazy(() => import("../pages/Blog/Blog"));
 
-export const ROUTES_ARR  = [
-    { name: "Home", component: Home, path: "/" },
-    { name: "Shop", component: "", path: "/shop" },
-    // { name: "Blog", component: "", path: "/blog" },
-    // { name: "FAQs", component: "", path: "/faqs" },
-]
+export const ROUTES_ARR = [
+  { name: "Home", component: Home, path: "/" },
+  { name: "Shop", component: "", path: "/shop" },
+  { name: "Blog", component: Blog, path: "/blog" },
+  // { name: "FAQs", component: "", path: "/faqs" },
+];
 
 export const ABOUT_ROUTES = [
-    { name: "Company", component: "", path: "/" },
-    { name: "Careers", component: "", path: "/" },
-    { name: "Blogs", component: "", path: "/blog" },  
-]
+  { name: "Company", component: "", path: "/" },
+  { name: "Careers", component: "", path: "/" },
+  { name: "Blogs", component: "", path: "/blog" },
+];
 
 export const SUPPORT_ROUTES = [
-    { name: "Contact Support", component: "", path: "/" },
-    { name: "Help Resources", component: "", path: "/" },
-    { name: "Release Updates", component: "", path: "/" },  
-]
+  { name: "Contact Support", component: "", path: "/" },
+  { name: "Help Resources", component: "", path: "/" },
+  { name: "Release Updates", component: "", path: "/" },
+];
 
 export const PLATFORM_ROUTES = [
-    { name: "Terms & Privacy", component: "", path: "/" },
-    { name: "Pricing", component: "", path: "/" },
-]
+  { name: "Terms & Privacy", component: "", path: "/" },
+  { name: "Pricing", component: "", path: "/" },
+];
 
 export const HELP_ROUTES = [
-    { name: "Send a Message", component: "", path: "/" },
-    { name: "Request a Quote", component: "", path: "/" },
-    { name: "FAQs", component: "", path: "/" },   
-]
+  { name: "Send a Message", component: "", path: "/" },
+  { name: "Request a Quote", component: "", path: "/" },
+  { name: "FAQs", component: "", path: "/" },
+];
